012739d17c3ecfd5812ae1240837604f354a34f5
fred_feuerstein <fred_feuerstein@herr-der-mails.de>
2017-09-04 12:47:45 +0200

    code-cleaning, additional dvb-t2 symlinks, external changelog-file logo-updater

--- logo-addon/logo-addon.sh
+++ logo-addon/logo-addon.sh
@@ -17,22 +17,6 @@
 # Dort ist auch zusätzlich bei Bedarf ein Radio-Senderlogo-Paket zu finden.
 #
 #
-# Changelog:
-# 0.20 = Änderung vom Basis-Skript auf statische Version, alle Variablen werden
-#        über das Update ausgeführt
-# 0.18 = kleine Menü-Änderungen
-# 0.17 = Auswahl ob Logos intern / extern (logo-hdd-dir) gespeichert werden
-# 0.16 = Logo-Updater kann nun auch vor dem Start wieder beendet werden
-# 0.15 = Anpassungen an Update-Skript
-# 0.14 = Anpassungen an Update-Skript
-# 0.13 = Anpassungen an Update-Skript
-# 0.05 = Download-URL und Dateiname angepasst (NG -> NI)
-# 0.04 = Marginale Ausgabe-Änderungen
-# 0.03 = kleine Änderungen
-# 0.02 = bisher konnten nur Logos upgedated werden, nun
-#        koennen auch neue Symlinks angelegt werden.
-# 0.01 = Startversion
-#
 ###############################################################################
 
 archive="ni_zusatzlogos.zip"
