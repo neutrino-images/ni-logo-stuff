f727d9491c1b21fff22c68b4a26dae6627f08736
fred_feuerstein <fred_feuerstein@herr-der-mails.de>
2018-10-02 18:21:07 +0200

    changed https address from https://neutrino-images.de... to https://www.neutrino-images.de...

--- logo-addon/logo-addon.sh
+++ logo-addon/logo-addon.sh
@@ -32,7 +32,7 @@ cleanup
 
 # wget OR curl download:
 # cd /tmp && wget -q http://www.neutrino-images.de/channellogos/$archive
-cd /tmp && curl -ks https://neutrino-images.de/channellogos/$archive --output $archive
+cd /tmp && curl -ks https://www.neutrino-images.de/channellogos/$archive --output $archive
 
 if [ -e $archive ]; then
 	mkdir $workdir
